0000d5b7ba3bb12e2a944761a6e6271e4ea265d8
chenlong-xcy <chenlonghyl@163.com>
2023-03-22 19:07:22 +0800

    memcpy函数测试

--- string_memcpy/string_memcpy/test.c
+++ string_memcpy/string_memcpy/test.c
@@ -38,9 +38,29 @@
 
 
 #include<stdio.h>
+#include<assert.h>
 #include<string.h>
+void* my_memcpy(void* dest, const void* src, size_t num)
+{
+	assert(dest != NULL);//断言，当dest为空指针时报错
+	assert(src != NULL);//断言，当src为空指针时报错
+	void* ret = dest;//保存dest，便于之后返回
+	while (num--)//循环num次，每次拷贝1个字节
+	{
+		*(char*)dest = *(char*)src;
+		++(char*)dest;//((char*)dest)++;
+		++(char*)src;//((char*)src)++;
+	}
+	return ret;
+}
 int main()
 {
-	int arr1[] = { 1, 2, 3, 4, 5, 6, 7 };
-	memcpy(arr1 + 1, arr1, 8);
+	int arr1[] = { 1, 2, 3, 4, 5, 6 };
+	int arr2[10] = { 0 };
+	my_memcpy(arr2, arr1, 16);
+	for (int i = 0; i < sizeof(arr2) / sizeof(int); i++) {
+		printf("%d ", *(arr2 + i));
+	}
+	printf("\n");
+	return 0;
 }
